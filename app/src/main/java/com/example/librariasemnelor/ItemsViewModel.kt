23c2bc429e4321ed305fba6e2203fff55c5ac572
Andrei <andrei.n53@yahoo.com>
2022-01-01 18:08:13 +0200

    V03Libraria

---
+++ app/src/main/java/com/example/librariasemnelor/ItemsViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.librariasemnelor
+
+data class ItemsViewModel(val image: Int, val text: String) {
+}
--- app/src/main/java/com/example/librariasemnelor/SecondActivity.kt
+++ app/src/main/java/com/example/librariasemnelor/SecondActivity.kt
@@ -1,17 +1,19 @@
 package com.example.librariasemnelor;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.os.Bundle;
+
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button
 import android.widget.ListView;
-
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class SecondActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
@@ -23,6 +25,28 @@ class SecondActivity : AppCompatActivity() {
 
             actionBar.setDisplayHomeAsUpEnabled(true)
         }
+
+        // getting the recyclerview by its id
+        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
+
+        // this creates a vertical layout Manager
+        recyclerview.layoutManager = LinearLayoutManager(this)
+
+        // ArrayList of class ItemsViewModel
+        val data = ArrayList<ItemsViewModel>()
+
+        // This loop will create 20 Views containing
+        // the image with the count of view
+        for (i in 1..20) {
+            data.add(ItemsViewModel(R.drawable.ic_baseline_folder_24, "Item " + i))
+
+        }
+
+        // This will pass the ArrayList to our Adapter
+        val adapter = CustomAdapter(data)
+
+        // Setting the Adapter with the recyclerview
+        recyclerview.adapter = adapter
 
     }
 
